fdf1b3f50601897b86124865a93ebc49e4fbf50e
Tyler Wulff <tmwulff13@vt.edu>
2023-04-26 20:53:25 -0400

    Add Mod

---
+++ StS-DefaultModBase-master/theDefault/src/main/java/DBMod/DefaultMod.java
@@ -0,0 +1,221 @@
+package DBMod;
+
+import basemod.*;
+import basemod.interfaces.*;
+import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.shop.ShopScreen;
+import com.megacrit.cardcrawl.shop.StorePotion;
+import com.megacrit.cardcrawl.shop.StoreRelic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+// Please don't just mass replace "theDefault" with "yourMod" everywhere.
+// It'll be a bigger pain for you. You only need to replace it in 4 places.
+// I comment those places below, under the place where you set your ID.
+
+//TODO: FIRST THINGS FIRST: RENAME YOUR PACKAGE AND ID NAMES FIRST-THING!!!
+// Right click the package (Open the project pane on the left. Folder with black dot on it. The name's at the very top) -> Refactor -> Rename, and name it whatever you wanna call your mod.
+// Scroll down in this file. Change the ID from "theDefault:" to "yourModName:" or whatever your heart desires (don't use spaces). Dw, you'll see it.
+// In the JSON strings (resources>localization>eng>[all them files] make sure they all go "yourModName:" rather than "theDefault", and change to "yourmodname" rather than "thedefault".
+// You can ctrl+R to replace in 1 file, or ctrl+shift+r to mass replace in specific files/directories, and press alt+c to make the replace case sensitive (Be careful.).
+// Start with the DefaultCommon cards - they are the most commented cards since I don't feel it's necessary to put identical comments on every card.
+// After you sorta get the hang of how to make cards, check out the card template which will make your life easier
+
+/*
+ * With that out of the way:
+ * Welcome to this super over-commented Slay the Spire modding base.
+ * Use it to make your own mod of any type. - If you want to add any standard in-game content (character,
+ * cards, relics), this is a good starting point.
+ * It features 1 character with a minimal set of things: 1 card of each type, 1 debuff, couple of relics, etc.
+ * If you're new to modding, you basically *need* the BaseMod wiki for whatever you wish to add
+ * https://github.com/daviscook477/BaseMod/wiki - work your way through with this base.
+ * Feel free to use this in any way you like, of course. MIT licence applies. Happy modding!
+ *
+ * And pls. Read the comments.
+ */
+
+@SpireInitializer
+public class DefaultMod implements
+        PostBattleSubscriber,
+        PostDrawSubscriber,
+        OnStartBattleSubscriber,
+        OnCardUseSubscriber,
+        PostPotionUseSubscriber,
+        PostCampfireSubscriber,
+        PostCreateShopPotionSubscriber,
+        PostCreateShopRelicSubscriber,
+        PotionGetSubscriber{
+    // Make sure to implement the subscribers *you* are using (read basemod wiki). Editing cards? EditCardsSubscriber.
+    // Making relics? EditRelicsSubscriber. etc., etc., for a full list and how to make your own, visit the basemod wiki.
+    public static final Logger logger = LogManager.getLogger(DefaultMod.class.getName());
+    private static String modID;
+
+    // Mod-settings settings. This is if you want an on/off savable button
+    public static Properties theDefaultDefaultSettings = new Properties();
+    public static final String ENABLE_PLACEHOLDER_SETTINGS = "enablePlaceholder";
+    public static boolean enablePlaceholder = true; // The boolean we'll be setting on/off (true/false)
+    int floor = 0;
+    // =============== /MAKE IMAGE PATHS/ =================
+    
+    // =============== /INPUT TEXTURE LOCATION/ =================
+    
+    
+    // =============== SUBSCRIBE, CREATE THE COLOR_GRAY, INITIALIZE =================
+    
+    public DefaultMod() {
+        logger.info("Subscribe to BaseMod hooks");
+        
+        BaseMod.subscribe(this);
+        
+      /*
+           (   ( /(  (     ( /( (            (  `   ( /( )\ )    )\ ))\ )
+           )\  )\()) )\    )\()))\ )   (     )\))(  )\()|()/(   (()/(()/(
+         (((_)((_)((((_)( ((_)\(()/(   )\   ((_)()\((_)\ /(_))   /(_))(_))
+         )\___ _((_)\ _ )\ _((_)/(_))_((_)  (_()((_) ((_|_))_  _(_))(_))_
+        ((/ __| || (_)_\(_) \| |/ __| __| |  \/  |/ _ \|   \  |_ _||   (_)
+         | (__| __ |/ _ \ | .` | (_ | _|  | |\/| | (_) | |) |  | | | |) |
+          \___|_||_/_/ \_\|_|\_|\___|___| |_|  |_|\___/|___/  |___||___(_)
+      */
+      
+        setModID("DBMod");
+        // cool
+        // TODO: NOW READ THIS!!!!!!!!!!!!!!!:
+        
+        // 1. Go to your resources folder in the project panel, and refactor> rename theDefaultResources to
+        // yourModIDResources.
+        
+        // 2. Click on the localization > eng folder and press ctrl+shift+r, then select "Directory" (rather than in Project) and press alt+c (or mark the match case option)
+        // replace all instances of theDefault with yourModID, and all instances of thedefault with yourmodid (the same but all lowercase).
+        // Because your mod ID isn't the default. Your cards (and everything else) should have Your mod id. Not mine.
+        // It's important that the mod ID prefix for keywords used in the cards descriptions is lowercase!
+
+        // 3. Scroll down (or search for "ADD CARDS") till you reach the ADD CARDS section, and follow the TODO instructions
+
+        // 4. FINALLY and most importantly: Scroll up a bit. You may have noticed the image locations above don't use getModID()
+        // Change their locations to reflect your actual ID rather than theDefault. They get loaded before getID is a thing.
+        
+        logger.info("Done subscribing");
+        
+        logger.info("Creating the color ");
+
+        logger.info("Done creating the color");
+        
+        
+        logger.info("Adding mod settings");
+        // This loads the mod settings.
+        // The actual mod Button is added below in receivePostInitialize()
+        theDefaultDefaultSettings.setProperty(ENABLE_PLACEHOLDER_SETTINGS, "FALSE"); // This is the default setting. It's actually set...
+        try {
+            SpireConfig config = new SpireConfig("defaultMod", "theDefaultConfig", theDefaultDefaultSettings); // ...right here
+            // the "fileName" parameter is the name of the file MTS will create where it will save our setting.
+            config.load(); // Load the setting and set the boolean to equal it
+            enablePlaceholder = config.getBool(ENABLE_PLACEHOLDER_SETTINGS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        logger.info("Done adding mod settings");
+        
+    }
+    
+    // ====== NO EDIT AREA ======
+    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
+    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
+    
+    public static void setModID(String ID) { // DON'T EDIT
+        logger.info("You seeing?");
+        Gson coolG = new Gson(); // EY DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf(StandardCharsets.UTF_8)); // i hate u Gdx.files
+        InputStream in = DefaultMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT THIS ETHER
+
+        logger.info("Success! ID is " + modID); // WHY WOULD U WANT IT NOT TO LOG?? DON'T EDIT THIS.
+    } // NO
+    
+    public static String getModID() { // NO
+        return modID; // DOUBLE NO
+    } // NU-UH
+
+    // ====== YOU CAN EDIT AGAIN ======
+
+
+    public static void initialize() {
+        logger.info("========================= Initializing Default Mod. Hi. =========================");
+        DefaultMod defaultmod = new DefaultMod();
+        logger.info("========================= /Default Mod Initialized. Hello World./ =========================");
+    }
+
+
+    // =============== / POST-INITIALIZE/ =================
+
+    // ================ ADD POTIONS ===================
+    @Override
+    public void receivePotionGet(AbstractPotion p) {
+        System.out.println("Received potion: " + p.name);
+    }
+    // ================ /ADD POTIONS/ ===================
+    @Override
+    public void receivePostPotionUse(AbstractPotion p) {
+        System.out.println("THIS POTION" + p.name);
+    }
+    // ================ ADD RELICS ===================
+    @Override
+    public void receivePostBattle(AbstractRoom r) {
+        logger.info("This is the room from the battle:  " + r);
+    }
+    @Override
+    public void receiveCreateShopPotions(ArrayList<StorePotion> s, ShopScreen p){
+        System.out.println("Shop Potions");
+    }
+    @Override
+    public boolean receivePostCampfire(){
+        System.out.println("Post Campfire");
+        return true;
+    }
+    @Override
+    public void receiveOnBattleStart(AbstractRoom r){
+        logger.info("Battle started" );
+    }
+    @Override
+    public void receiveCreateShopRelics(ArrayList<StoreRelic> r, ShopScreen s){
+    }
+    @Override
+    public void receiveCardUsed(AbstractCard card) {
+        System.out.println(card.name + " USED");
+    }
+    @Override
+    public void receivePostDraw(AbstractCard card) {
+        logger.info(card.name + " was drawn!");
+    }
+
+    // ================ /ADD RELICS/ ===================
+
+
+    // ================ ADD CARDS ===================
+    
+    // ================ /ADD CARDS/ ===================
+    
+    
+    // ================ LOAD THE TEXT ===================
+
+    // ================ /LOAD THE TEXT/ ===================
+
+    // ================ LOAD THE KEYWORDS ===================
+
+
+    // ================ /LOAD THE KEYWORDS/ ===================
+
+}
